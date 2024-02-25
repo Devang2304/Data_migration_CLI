da9e897eb0331c6b821bf05503eefd24ee6b2397
Devang2304 <devangvartak2304@gmail.com>
2024-02-25 17:51:33 +0530

    added dotenv to avoid timewaste

--- OnPremiseToMongoCloud.js
+++ OnPremiseToMongoCloud.js
@@ -8,6 +8,7 @@ const {connection} = mongoose;
 // secret key = 'd56d4a043edc26d19401805bbc3a022fcf6de4c49ae2d8ad491f3a97cb325a315e038923d259a9d1b9ff5dcc200c821f6a31d37aeae3aea4f2288eb7f8f4bcb4f9632e03fc57331fe0466aebe89b3fec73a284a6e782382973ca4aaa51709ca2ba286db5418199aeca2b4e33c13a41cd731b36271a16be6e3ad1cce0b398d'
 
 // decrypting the file before sending it to mongoDB
+
 const migrateToMongo=async(CONNECTION_URL,ecryptedFilePath,secretKey,dbName,collectionName)=>{
     const dBInstance = await connectDB(CONNECTION_URL);
     const collection = dBInstance.db(dbName).collection(collectionName);
@@ -15,6 +16,17 @@ const migrateToMongo=async(CONNECTION_URL,ecryptedFilePath,secretKey,dbName,coll
     const fileData=fs.readFileSync(ecryptedFilePath,'utf8');
     const dataInserted=await collection.insertMany(JSON.parse(fileData));
     console.log("Data migrated to cloud successfully");
+    connection.close();
+    process.exit();
+}
+
+// function for migrating data from mysql to mongodb
+export const migrateSQLtoMongo = async (CONNECTION_URL, FilePath, dbName, collectionName) => {
+    const dBInstance = await connectDB(CONNECTION_URL);
+    const collection = dBInstance.db(dbName).collection(collectionName);
+    const fileData = fs.readFileSync(FilePath, 'utf8');
+    const dataInserted = await collection.insertMany(JSON.parse(fileData));
+    console.log("Data migrated from SQL  to mongoose atlas successfully");
     connection.close();
     process.exit();
 }
--- mySQLToMongoDB.js
+++ mySQLToMongoDB.js
@@ -1,6 +1,8 @@
 import mysql from 'mysql2/promise';
 import fs from 'fs';
-
+import { migrateSQLtoMongo } from './OnPremiseToMongoCloud.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
   const mySQLToMongoDB= async (userName,database, password, table, output) => {
     try {
@@ -29,6 +31,16 @@ import fs from 'fs';
         await connection.end();
         await fs.writeFileSync(output, JSON.stringify(jsonData[0], null, 2));
         console.log(`Successfully converted to JSON: ${output}`);
+
+        console.log('Migrating into MongoDB...');
+        // CONNECTION_URL, FilePath, dbName, collectionName
+        try {
+          await migrateSQLtoMongo(process.env.CONNECTION_URL,'output.json','testingdb','testingCollection2');
+          console.log('Migration complete!');
+        } catch (error) {
+          console.log('Error while migrating to MongoDB,pls try again later',error);
+        }
+
       } catch (err) {
         console.error(`Error converting table: ${err}`);
       }
@@ -36,4 +48,4 @@ import fs from 'fs';
   
 
 
-  mySQLToMongoDB('','testingdb','','testingtable','output.json');
+  mySQLToMongoDB('','testingdb',process.env.mySQLpassword,'testingtable','output.json');
